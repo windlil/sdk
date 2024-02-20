5676bc1fc7a5438d376322f784fa57f7e2135a90
windlil <spacesouls@163.com>
2024-02-20 17:02:33 +0800

    chore: update

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'head'
+    })
+  ]
+}
